23230e8b287d22dc904e3d656dd718c1f4097dcc
oliverkirsch <oliver.kirsch@tn.mischok.academy>
2025-03-04 11:51:09 +0100

    initial commit

---
+++ src/main/java/com/example/fotowettbewerb/FotowettbewerbApplication.java
@@ -0,0 +1,13 @@
+package com.example.fotowettbewerb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotowettbewerbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotowettbewerbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fotowettbewerb/FotowettbewerbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fotowettbewerb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotowettbewerbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
